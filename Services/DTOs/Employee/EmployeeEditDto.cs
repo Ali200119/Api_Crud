80c3cc6112852bd1aea120d2538d91d9bc3ebee1
Ali200119 <aliit@code.edu.az>
2023-05-17 17:57:29 +0400

    feat: Employee Update

---
+++ Services/DTOs/Employee/EmployeeEditDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Services.DTOs.Employee
+{
+	public class EmployeeEditDto
+	{
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
